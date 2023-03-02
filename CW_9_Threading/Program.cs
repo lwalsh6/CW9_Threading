8d3b21013bd20749ec9f4019f53ec41d94f04d28
Logan Walsh <lwalsh6@ut.utm.edu>
2023-03-02 13:43:05 -0600

    Add project files.

---
+++ CW_9_Threading/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Logan Walsh
+//3-2-2023
+//This is a threading simulation for a dartboard
+
+namespace CW_9_Threading
+{
+    public class FindPiThread
+    {
+        int dartsThrow;
+        int dartsCount;
+        Random rand;
+        FindPiThread(int dT)
+        {
+            rand = new Random();
+            int dartsThrow = dT;
+
+        }
+        public void throwDarts()
+        {
+
+        }
+    }
+    
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
