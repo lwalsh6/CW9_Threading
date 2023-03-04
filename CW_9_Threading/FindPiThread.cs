02e3900bc8828b35df5bfa20258f3170d94e62db
Logan Walsh <lwalsh6@ut.utm.edu>
2023-03-04 17:18:56 -0600

    IDK What is wrong with this program

---
+++ CW_9_Threading/FindPiThread.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW_9_Threading
+{
+    public class FindPiThread
+    {
+        int dartsThrow;
+        int dartsCount;
+        Random rand;
+        public FindPiThread(int dThrow)
+        {
+            rand = new Random();
+            int dartsThrow = dThrow;
+            dartsCount = 0;
+            Console.WriteLine(dThrow);
+            Console.WriteLine(dartsThrow);
+        }
+        public int getScore()
+        {
+            return dartsCount;
+        }
+        public void throwDarts()
+        {
+
+            double y;
+            double x;
+
+            for (int i = 1; i <= dartsThrow; i++)
+            {
+                y = rand.NextDouble();
+                x = rand.NextDouble();
+                double points = Math.Sqrt((x * x) + (y * y));
+                if (points < 1)
+                {
+                    dartsCount++;
+                }
+            }
+        }
+    }
+}
--- CW_9_Threading/Program.cs
+++ CW_9_Threading/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 //Logan Walsh
 //3-2-2023
@@ -9,28 +12,47 @@ using System.Threading.Tasks;
 
 namespace CW_9_Threading
 {
-    public class FindPiThread
-    {
-        int dartsThrow;
-        int dartsCount;
-        Random rand;
-        FindPiThread(int dThrow)
-        {
-            rand = new Random();
-            int dartsThrow = dThrow;
-
-        }
-        public void throwDarts()
-        {
-            
-        }
-    }
-    
-
     internal class Program
     {
         static void Main(string[] args)
         {
+            int dartInt;
+            int threadInt;
+
+            Console.Write("How many darts do you want to throw for each round: ");
+            dartInt = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("How many rounds do you want to run: ");
+            threadInt = Convert.ToInt32(Console.ReadLine());
+
+            List<Thread> roundThread = new List<Thread>();
+            List<FindPiThread> piThread = new List<FindPiThread>();
+
+            for(int i = 0; i < threadInt; i++)
+            {
+                FindPiThread find = new FindPiThread(dartInt);
+                piThread.Add(find);
+                Thread newPiThread = new Thread(new ThreadStart(find.throwDarts));
+                roundThread.Add(newPiThread);
+                newPiThread.Start();
+                Thread.Sleep(16);
+            }
+            foreach(var Thread in roundThread)
+            {
+                Thread.Join();
+            }
+
+            double count = 0;
+            foreach(var FindPiThread in piThread)
+            {
+                count += FindPiThread.getScore();
+            }
+
+            double averageDenom = dartInt * threadInt;
+            double average = (count * 4) / (averageDenom);
+
+            Console.WriteLine("Average Score: " + average);
+            Console.ReadKey();
         }
     }
 }
